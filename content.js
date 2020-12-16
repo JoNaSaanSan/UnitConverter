ddc2bfa0681b3aaf27333353d715bb6f0dea62ef
Jonathan <Jonathan.Haudenschild@gmail.com>
2020-12-16 19:48:59 +0100

    start

---
+++ content.js
@@ -0,0 +1,5 @@
+if (document.body.innerHTML = 'cup'){
+    document.body.innerHTML = document.body.innerHTML.replace(/cup/g, 'cup dog');
+}
+
+document.body.innerHTML = document.body.innerHTML.replace(/cup/g, 'cup dog');
